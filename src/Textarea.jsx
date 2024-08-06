c00af2650a78b025d5aa6ecea8307e59139f8df0
MargaretLai <MargaretLUI01@gmail.com>
2024-08-06 18:54:18 -0400

    Added hook to control user's text input

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import { useState } from "react";
 
 export default function Textarea() {
+  const [text, setText] = useState("");
+
   return (
     <textarea
+      value={text}
       className="textarea"
       placeholder="Enter your text"
       spellCheck="false"
       onChange={(e) => {
-        console.log(e.target.value);
+        setText(e.target.value);
       }}
     ></textarea>
   );
