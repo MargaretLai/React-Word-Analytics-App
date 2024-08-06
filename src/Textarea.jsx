568c00d5dddf8c698f8b8309565f9b098d76a625
MargaretLai <MargaretLUI01@gmail.com>
2024-08-06 19:44:00 -0400

    (For security) Added function to prevent user from entering script

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -3,15 +3,23 @@ import { useState } from "react";
 export default function Textarea() {
   const [text, setText] = useState("");
 
+  function handleChange(e) {
+      let txt = e.target.value;
+      if (txt.includes("script")) {
+        alert("You can't enter script tag");
+      }
+      txt = txt.replace("script", " ");
+
+      setText(txt);
+  }
+
   return (
     <textarea
       value={text}
       className="textarea"
       placeholder="Enter your text"
       spellCheck="false"
-      onChange={(e) => {
-        setText(e.target.value);
-      }}
+      onChange={handleChange}
     ></textarea>
   );
 }
