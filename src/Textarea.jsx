a363a2f614c623c3b3b0fbaf687cd4c14d278b73
MargaretLai <MargaretLUI01@gmail.com>
2024-08-06 18:45:01 -0400

    Added function to access user input

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function Textarea() {
-  return <textarea className="textarea">Textarea</textarea>;
+  return (
+    <textarea
+      className="textarea"
+      placeholder="Enter your text"
+      spellCheck="false"
+      onChange={(e) => {
+        console.log(e.target.value);
+      }}
+    ></textarea>
+  );
 }
