b2aa62c30646ccc77f16c1b1a6f2b7eee47a9d31
MargaretLai <MargaretLUI01@gmail.com>
2024-08-07 16:25:25 -0400

    Lifted state up to show the change of characters

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,12 +1,16 @@
-import React from "react";
 import Textarea from "./Textarea.jsx";
 import Stats from "./Stats.jsx";
+import { useState } from "react";
 
 export default function Container() {
+  const [text, setText] = useState("");
+  const numOfCharacters = text.length;
+  console.log(numOfCharacters)
+
   return (
     <main className="container">
-      <Textarea />
-      <Stats />
+      <Textarea text={text} setText={setText} />
+      <Stats numofCharacters={numOfCharacters}/>
     </main>
   );
 }
--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,22 +1,19 @@
-export default function Stats() {
+export default function Stats({ numofCharacters }) {
   return (
     <section className="stats">
-      <section className="stat">
-        <span className="stat__number">0</span>
-        <h2 className="second-heading">Words</h2>
-      </section>
-      <section className="stat">
-        <span className="stat__number">0</span>
-        <h2 className="second-heading">Characters</h2>
-      </section>
-      <section className="stat">
-        <span className="stat__number">280</span>
-        <h2 className="second-heading">Instagram</h2>
-      </section>
-      <section className="stat">
-        <span className="stat__number">2200</span>
-        <h2 className="second-heading">Facebook</h2>
-      </section>
+      <Stat num={0} label={"Words"} />
+      <Stat num={numofCharacters} label={"Characters"} />
+      <Stat num={280} label={"Instagram"} />
+      <Stat num={2200} label={"Facebook"} />
+    </section>
+  );
+}
+
+function Stat({ num, label }) {
+  return (
+    <section className="stat">
+      <span className="stat__number">{num}</span>
+      <h2 className="second-heading">{label}</h2>
     </section>
   );
 }
