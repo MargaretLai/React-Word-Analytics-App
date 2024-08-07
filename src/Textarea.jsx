fd06c2e7a2926711557e9f2e938e5ba48ca337ef
MargaretLai <MargaretLUI01@gmail.com>
2024-08-06 20:40:38 -0400

    Added Warning component to prevent user from entering script and @

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,25 +1,35 @@
 import { useState } from "react";
+import Warning from "./Warning";
 
 export default function Textarea() {
   const [text, setText] = useState("");
+  const [warningText, setWarningText] = useState("");
 
   function handleChange(e) {
-      let txt = e.target.value;
-      if (txt.includes("script")) {
-        alert("You can't enter script tag");
-      }
+    let txt = e.target.value;
+    if (txt.includes("script")) {
       txt = txt.replace("script", " ");
+      setWarningText("No script tag allowed!");
+    } else if (txt.includes("@")) {
+      txt = txt.replace("@", " ");
+      setWarningText("No @ symbol allowed!");
+    } else {
+      setWarningText("");
+    }
 
-      setText(txt);
+    setText(txt);
   }
 
   return (
-    <textarea
-      value={text}
-      className="textarea"
-      placeholder="Enter your text"
-      spellCheck="false"
-      onChange={handleChange}
-    ></textarea>
+    <div className="textarea">
+      <textarea
+        value={text}
+        className="textarea"
+        placeholder="Enter your text"
+        spellCheck="false"
+        onChange={handleChange}
+      ></textarea>
+      <Warning warningText={warningText} />
+    </div>
   );
 }
